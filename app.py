55e935c3ad972a79680642eb6f69e17dbfe11e17
Enrique Mattos <100850499+evmpython@users.noreply.github.com>
2026-05-08 14:43:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-# https://www.youtube.com/watch?v=TMtjnQ4sPTE
 
+# importando o GEE e Geemap
+import ee
+import geemap
 import streamlit as st
+
+# inicializando GEE
+geemap.ee_initialize(project='testepaulo-495717')
+
 st.markdown('# Testando o Streamlit by Enriqueddddd2222')
 
+# cria o mapa com centro nas coordenadas [latitude, longitude] e zoom inicial
+Map = geemap.Map(center=[-14, -55], zoom=4)
 
+# adicionando basemap
+Map.add_basemap('SATELLITE')
 
+# exibe na tela
+Map
